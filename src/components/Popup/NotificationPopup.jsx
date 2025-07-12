f593446528642eddbeadbf87891e94d48209c4eb
kgs9843 <kgs9843@naver.com>
2025-07-12 20:16:13 +0000

    [feat] alert popup

--- src/components/Popup/NotificationPopup.jsx
+++ src/components/Popup/NotificationPopup.jsx
@@ -12,7 +12,7 @@ const NotificationPopup = ({ onClose }) => {
   }, []);
   return (
     <div className="absolute w-full h-full inset-0 subColor flex-1 px-4 flex flex-col z-50">
-      <div className="w-full h-10 mt-15 font-bold text-2xl text-end flex flex-row justify-between">
+      <div className="w-full h-10 mb-5 mt-15 font-bold text-2xl text-end flex flex-row justify-between">
         <img
           src={Back3DBtnIcon}
           alt="뒤로가기 버튼"
@@ -20,6 +20,14 @@ const NotificationPopup = ({ onClose }) => {
           onClick={onClose}
         />
         <div>알림</div>
+      </div>
+      <div className="w-full rounded-2xl bg-white h-15 flex flex-col justify-center px-3 items-start">
+        <div className="w-auto h-auto font-bold text-xl">
+          '구름 식당' 도장이 찍혔어요.
+        </div>
+        <div className="w-auto h-auto textGrayColor font-bold text-xs">
+          2025-07-13
+        </div>
       </div>
     </div>
   );
